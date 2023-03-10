a7b637ba8f8c1e2641393f10577ce5ca7c14b0b2
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-03-10 16:10:52 +0700

    creat servlet

---
+++ user-manager-demo-git/src/main/java/com/example/controller/UserServlet.java
@@ -0,0 +1,8 @@
+package com.example.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(value = "/user")
+public class UserServlet extends HttpServlet {
+}
