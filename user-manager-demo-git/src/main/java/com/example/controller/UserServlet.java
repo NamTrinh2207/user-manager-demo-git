35329a24d5ad72ca545546d0676ba09ca8d9364c
Tuong <aumanhtuong2001@gmail.com>
2023-03-10 17:23:45 +0700

    show list

--- user-manager-demo-git/src/main/java/com/example/controller/UserServlet.java
+++ user-manager-demo-git/src/main/java/com/example/controller/UserServlet.java
@@ -1,14 +1,20 @@
 package com.example.controller;
 
+import com.example.model.User;
+import com.example.service.UserIplm;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(value = "/user")
 public class UserServlet extends HttpServlet {
+    private UserIplm userIplm = new UserIplm();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -24,6 +30,18 @@ public class UserServlet extends HttpServlet {
             case "delete":
                 break;
             default:
+                listForm(req, resp);
+        }
+    }
+
+    private void listForm(HttpServletRequest req, HttpServletResponse resp) {
+        List<User> users = this.userIplm.findAll();
+        req.setAttribute("listUser", users);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("user/list.jsp");
+        try {
+            dispatcher.forward(req, resp);
+        } catch (ServletException | IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
